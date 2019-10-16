6559f264cd4120984ccee269cad202c62c933df0
Alvin Balmeo <albalmeo@gmail.com>
2019-10-15 21:52:23 -0700

    Adding todos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // used in html element in index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-crash0todolist';
+  name: string = 'Alvin';
+
+  constructor() {
+    this.changeName('John');
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+  }
 }
